f860c106a83794680aa0777cda271a9a94c0ff44
eyobScript <eyobbmulugeta@gmail.com>
2025-04-11 23:12:47 +0300

    gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'screens/calculator.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.blue,
-    ),
-  ));
+  runApp(const CalculatorApp());
 }
 
+class CalculatorApp extends StatelessWidget {
+  const CalculatorApp({super.key});
 
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white, // White background
+      ),
+      home: const CalculatorScreen(),
+    );
+  }
+}
